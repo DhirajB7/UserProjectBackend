cc27e09feacfd4901c5886d5d4a13e713616390f
dhirajb7 <bond.dhiraj@gmail.com>
2020-06-05 12:15:03 +0530

    first commit

---
+++ routers/UserRouter.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../Model/UserModel')
+
+//Get All Users
+router.get('/', async (req,res) => {
+   try {
+
+    const allUsers = await User.find()
+
+    res.json(allUsers)
+       
+   } catch (error) {
+       res.send(error)
+   }
+})
+
+//Get One User
+
+router.get('/:id', async (req,res) => {
+    try {
+ 
+     const oneUserGet = await User.findById(req.params.id)
+ 
+     res.json(oneUserGet)
+        
+    } catch (error) {
+        res.send(error)
+    }
+ })
+
+
+//Post A User
+
+router.post('/', async (req,res)=>{
+
+    const oneUser = new User({
+        name:req.body.name.toLowerCase(),
+        email:req.body.email.toLowerCase(),
+        role:req.body.role.toLowerCase(),
+        status:req.body.status
+    })
+
+    
+    try {
+
+        await oneUser.save()
+
+        res.send("USER ADDED "+oneUser.name)
+        
+    } catch (error) {
+
+        res.send(error)
+        
+    }
+
+})
+
+//patch one user status
+
+router.patch('/status/:id', async (req,res) => {
+    try {
+ 
+     const oneUserPatch = await User.findById(req.params.id)
+     oneUserPatch.status = req.body.status
+    await oneUserPatch.save()
+ 
+     res.json(oneUserPatch)
+        
+    } catch (error) {
+        res.send(error)
+    }
+ })
+
+ //Delete user
+
+ router.delete('/:id', async (req,res) => {
+    try {
+ 
+    await User.findByIdAndDelete(req.params.id)
+ 
+     res.send("USER DELETED")
+        
+    } catch (error) {
+        res.send(error)
+    }
+ })
+
+
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,21 @@
+const Express = require('express')
+const Mongoose = require('mongoose')
+const url = "mongodb://localhost/UserDB"
+const UserRouter = require('./routers/UserRouter')
+
+/////////DataBase Connectivity ////////////
+
+Mongoose.connect(url,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})  //connetor
+Mongoose.connection.on('open',() => console.log("DB CONNECTED AND HANDLE READY"))          //handle
+
+/////////////Router part///////////////////
+
+const app = Express()
+
+app.use(Express.json())
+app.use("/user",UserRouter)
+
+app.listen(3001,()=>console.log("SERVER STARTED AT 3001"))
